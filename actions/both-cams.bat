d6fcbfe171136fb5b4c644c5d6a8436ea65a6ef9
Emmanuel Barreto <Emmanuel.Barretto@universalorlando.com>
2021-08-26 06:06:25 -0400

    Camera actions

---
+++ actions/both-cams.bat
@@ -0,0 +1,4 @@
+::::: GENERATED WITH NOOBS CMDR v1.0.2.0 :::::
+chcp 65001
+
+OBSCommand.exe /server=127.0.0.1:4444 /password="" /setvolume="Camera-2-Audio",50,10 /setvolume="Camera-1-Audio",50,10 /scene="Game View - Both Cams"
---
+++ actions/camera-1.bat
@@ -0,0 +1,4 @@
+::::: GENERATED WITH NOOBS CMDR v1.0.2.0 :::::
+chcp 65001
+
+OBSCommand.exe /server=127.0.0.1:4444 /password="" /setvolume="Camera-1-Audio",75,10 /setvolume="Camera-2-Audio",0,10 /scene="Game View - Team A Full"
